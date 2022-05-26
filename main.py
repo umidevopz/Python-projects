58584bf678719e7e97d48ecd239193a9ff63f57c
umidevopz <102044298+umidevopz@users.noreply.github.com>
2022-05-26 09:16:01 +0500

    Basic.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# text = 'this hamburger is'
-# buck = 'dollars'
-# price = 10
-# fuck = text + price + bucks
-# print(fuck)
-# there is no way to concatenate str to int
-#
-#Input tutorial
-# name = input('whats your name, dude?\n')
-# print('hello' + name)
-# work = input('what are you gonna do next today?\n')
-# print('this ' + work + ' takes a lot of time')
-birthYear = input('BIrth year:')
+import calendar
 
-age = 2022 - int(birthYear)
+mm = 4
+yy = 2002
+
+print(calendar.month(mm, yy))

d823ce1523299ad07a95d1c082ae2cf0e7254306
umidrahmatov18092002@gmail.com <umidevopz@gmail.com>
2022-05-28 16:49:59 +0500

    new lesson

---
+++ Python-Basics/math.py
@@ -0,0 +1,17 @@
+import math 
+
+pi = 3.14
+
+# print(round(pi)) it shows non-integer (butun son)
+
+# print(math.ceil(pi)) butun bo'lmagan sonni unga yaqin 'katta' butun songa o'zgartiradi
+
+# print(math.floor(pi)) butun bo'lmagan sonni unga yaqin 'kichik' butun songa o'zgartiradi
+
+# print(abs(pi)) abs() raqamni minut yoki plus qilib beradi +/-abs()
+
+# print(math.sqrt(16)) bu sonni darajasini korsatadi
+
+# list = [163, 7939,379378,8397383]
+# print(max(list)) max() eng katta raqamni korsatadi
+
